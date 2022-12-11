83f98db98426d2dacb0dc347fb3122f5b749d11e
solbiko <gsb0904@hanmail.net>
2022-12-11 11:31:53 +0900

    생성 목록 수정

--- src/views/survey/list/MakeList.js
+++ src/views/survey/list/MakeList.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+/* eslint-disable jsx-a11y/anchor-has-content */
+import React, { useState, useEffect, createRef } from 'react'
 import {
   CInputGroup,
   CDropdownToggle,
@@ -17,54 +18,52 @@ import {
   CCardFooter,
   CCardHeader,
   CBadge,
-  CPagination,
-  CPaginationItem,
 } from '@coreui/react'
 import axios from "axios";
 import apiConfig from 'src/lib/apiConfig';
 import usePromise from 'src/lib/usePromise';
-import moment from 'moment';
 import { useSelector } from 'react-redux';
 import Select from 'react-select';
 import makeAnimated from 'react-select/animated';
-import Loading from 'src/lib/Loading/Loading';
+import Loading from 'src/lib/Loading/Loading2';
 import { useNavigate } from 'react-router-dom';
+import Pagination from "react-js-pagination";
+import SurveyCard from './SurveyCard';
 
 const MakeList = () => {
-  const navigate = useNavigate();
 
-
-  const { user } = useSelector(({user})=> ({user:user.user}));
-  
-  let accessToken = null;
-  if(user == null){
-    navigate('/');
-
-  }else{
-    accessToken = user.token.access_token;
-  }
-
-
-  const current = decodeURI(window.location.href);
-  const search = current.split("?")[1];
-  const params = new URLSearchParams(search);
-  const nowPage = params.get('page') ? params.get('page') : 1;
-
+  const [loading, setLoading] = useState(false);
   const [selectedOption, setSelectedOption] = useState(null);
+  const [surveyList, setSurveyList] = useState([]);
+  const [pageData, setPageData] = useState({
+    totalPage: 0,
+    page: 1,
+    size: 0,
+    start: 0,
+    end: 0,
+    prev: false,
+    next: false,
+    totalElements : 1
+  })
+ 
+  useEffect(() => {
+    handleFetch(1);
+  }, []);
+  
 
   // 카테고리 리스트
   let categoryOptionList = [];
   const animatedComponents = makeAnimated();
-  const [cloading, cresponse, cerror] = usePromise(() => {
+  const [cloading, response, cerror] = usePromise(() => {
     return axios.get(apiConfig.surveyCategorySelectList)
   }, []);
-  if(cresponse != null){
-    cresponse.data.map((option) => {
+  if(response != null){
+    response.data.map((option) => {
       categoryOptionList.push({ value: option.surCatId, label: option.content });
     });
   }
 
-
+  // 선택된 카테고리 배열
   function getSelectedCategory(){
     let arr= [];
     if(selectedOption != null){
@@ -75,38 +74,30 @@ const MakeList = () => {
     return arr;
   }
 
-  let surveyList = [];
-  let page = {
-    prev: false,
-    start: 1,
-    page: 3,
-    next: false,
-    end: 1,
-    pagelist: [1],
-  }
-  const [loading, response, error] = usePromise(() => {
-    return axios.get(apiConfig.surveyMakeList + "?category="+getSelectedCategory()+"&page="+ nowPage,
-    {headers: {'Authorization': 'Bearer ' + accessToken }})
-  }, []);
+  // 설문 리스트
+  const handleFetch = (selectedPage) => {
+    axios.get(apiConfig.surveyMakeList + "?category="+getSelectedCategory()+"&page="+ selectedPage)
+    .then(response => {
+      const data = response.data;
+      setSurveyList(data.content);
+      setPageData({
+      totalPage: 0,
+      page: data.number+1,
+      size: data.size,
+      start: 1,
+      end: data.totalPages,
+      prev: data.first? false: true,
+      next: data.last? false: true,
+      totalElements : data.totalElements
+    }); 
+      setLoading(false);
+    })
+    .catch(error => console.error('Error', error));
+  };
 
-  if(response != null){
-    console.log(response);
-    surveyList = response.data.content;
-    page.page = response.data.number+1;
-    let arr = [];
-    for (let i = 0; i < response.data.totalPages; i++) {
-      arr.push(i+1);
-    }
-    page.pagelist = arr;
-  }
-
-  const DuplicateSurveyOnClickHandler = (e, link,sur_id) => {
-    window.location.href = link+'?copy='+sur_id;
-  }
-
-  const SurveyDetailOnClickHandler = (e, link,sur_id) => {
-    window.location.href = link+"/"+sur_id;
-  }
+  const handlePageChange = (selectedPage) => {
+    handleFetch(selectedPage);
+  };
 
   return (
     <>
@@ -135,64 +126,24 @@ const MakeList = () => {
                   </CInputGroup>
                 </CForm>
                 <CRow>
+                { loading ? <Loading /> : <>
                   {surveyList.map((data) => (
-                    <CCol xs={3} key={data.surId} >
-                      <CCard className="mb-4">
-                        <CCardHeader>{data.content}</CCardHeader>
-                        <CCardBody>
-                          <CCardTitle>{data.title}</CCardTitle>
-                          <CCardText>
-                            <small>{moment(new Date(data.regDt)).format('YYYY-MM-DD')}~ {moment(new Date(data.dueDt)).format('YYYY-MM-DD')}</small><br/>
-
-                            <CBadge color={ data.statusName == "제작" ?  "success" : (data.statusName == "배포" ?"primary" : "danger")}> {data.statusName}</CBadge>
-
-
-
-                          </CCardText>
-                        </CCardBody>
-                        <CCardFooter>
-                          <CButton color="primary" variant="outline" className="m-1" size="sm" onClick={(e) => SurveyDetailOnClickHandler(e, '#/survey/detail', data.surId)}>Details</CButton>
-                          { data.statusName != "제작" ? (  <CButton color="warning" variant="outline" className="m-1" size="sm" onClick={(e) => DuplicateSurveyOnClickHandler(e, '#/survey/create',data.sur_id)}>Copy</CButton>):null}
-                          { data.statusName == "제작" ? (  <CButton color="success" variant="outline" className="m-1" size="sm">Edit</CButton>):null}
-                          <CButton color="danger" variant="outline" className="m-1" size="sm">Remove</CButton>
-                        </CCardFooter>
-                      </CCard>
-                    </CCol>
+                   <SurveyCard key={data.surId} data={data} ></SurveyCard>
                   ))}
+                  </>
+                }
                 </CRow>
-                <CPagination aria-label="Page navigation example" align="center">
-                  {page.prev ? (
-                    <CPaginationItem aria-label="Previous">
-                      <span aria-hidden="true">
-                        <a href={'/#/survey/makeList?category=' + getSelectedCategory()+ '&page=' + page.start - 1}>&laquo;</a>
-                      </span>
-                    </CPaginationItem>
-                  ) : (
-                    <CPaginationItem aria-label="Previous" disabled>
-                      <span aria-hidden="true">&laquo;</span>
-                    </CPaginationItem>
-                  )}
-                  {page.pagelist.map((idx) =>
-                    idx === page.page ? (
-                      <CPaginationItem active>{page.page}</CPaginationItem>
-                    ) : (
-                      <CPaginationItem>
-                        <a href={ "#/survey/makeList?category=" + getSelectedCategory() +  "&page=" + idx} >{idx}</a>
-                      </CPaginationItem>
-                    ),
-                  )}
-                  {page.next ? (
-                    <CPaginationItem aria-label="Next">
-                      <span aria-hidden="true">
-                        <a href={'/#/survey/makeList?category=' +getSelectedCategory() + '&page=' + page.end + 1}>&raquo;</a>
-                      </span>
-                    </CPaginationItem>
-                  ) : (
-                    <CPaginationItem aria-label="Next" disabled>
-                      <span aria-hidden="true">&raquo;</span>
-                    </CPaginationItem>
-                  )}
-                </CPagination>
+
+                <Pagination
+                      activePage={pageData.page}
+                      itemsCountPerPage={pageData.size}
+                      totalItemsCount={pageData.totalElements}
+                      pageRangeDisplayed={10}
+                      prevPageText={"‹"}
+                      nextPageText={"›"}
+                      onChange={handlePageChange}
+                    />
+
               </CCardBody>
             </CCard>
           </CCardBody>
---
+++ src/views/survey/list/SurveyCard.js
@@ -0,0 +1,55 @@
+import React, {  } from 'react';
+import {
+
+    CButton,
+    CCard,
+    CCardImage,
+    CCardBody,
+    CCardTitle,
+    CCardText,
+    CCol,
+    CCardFooter,
+    CCardHeader,
+    CBadge,
+  } from '@coreui/react';
+  import moment from 'moment';
+  import ReactImg_2 from 'src/assets/images/test5.jpeg';
+
+  
+  const SurveyCard = ({data}) => {
+
+    const DuplicateSurveyOnClickHandler = (e, link,sur_id) => {
+        window.location.href = link+'?copy='+sur_id;
+      }
+    
+      const SurveyDetailOnClickHandler = (e, link,sur_id) => {
+        window.location.href = link+"/"+sur_id;
+      }
+
+      return (
+        <>
+        <CCol xs={3} >
+        <CCard className="mb-4">
+        <CCardHeader>{data.categoryContent}</CCardHeader>
+        <CCardImage orientation="top" src={data.surveImg == undefined || data.surveImg == null ? ReactImg_2 : data.statusName } />
+        <CCardBody>
+            <CCardTitle>{data.title}</CCardTitle>
+            <CCardText>
+            <small>{moment(new Date(data.regDt)).format('YYYY-MM-DD')}~ {moment(new Date(data.dueDt)).format('YYYY-MM-DD')}</small><br/>
+        
+            <CBadge color={ data.statusName == "제작" ?  "success" : (data.statusName == "배포" ?"primary" : "danger")}> {data.statusName}</CBadge>
+            </CCardText>
+        </CCardBody>
+        <CCardFooter>
+            <CButton color="primary" variant="outline" className="m-1" size="sm" onClick={(e) => SurveyDetailOnClickHandler(e, '#/survey/detail', data.surId)}>Details</CButton>
+            { data.statusName != "제작" ? (  <CButton color="warning" variant="outline" className="m-1" size="sm" onClick={(e) => DuplicateSurveyOnClickHandler(e, '#/survey/create',data.sur_id)}>Copy</CButton>):null}
+            { data.statusName == "제작" ? (  <CButton color="success" variant="outline" className="m-1" size="sm">Edit</CButton>):null}
+            <CButton color="danger" variant="outline" className="m-1" size="sm">Remove</CButton>
+        </CCardFooter>
+        </CCard>
+        </CCol>
+    </>
+    )
+  }
+
+  export default SurveyCard
